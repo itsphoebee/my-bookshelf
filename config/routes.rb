0190873adfed2fba53865f3d4e0273344ecde991
Phoebe Lau <phoebelau7@gmail.com>
2018-02-27 17:30:37 -0500

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :reading_lists
+  resources :reading_lists, only: [:index, :show, :new, :create, :edit, :update]
   devise_for :users
-  resources :books
-  resources :genres
+  resources :books, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  get '/users/:id', to: 'users#show'
+
+  resources :users, only: [:show] do
+  # nested resource for reading_lists
+    resources :reading_lists, only: [:show]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
