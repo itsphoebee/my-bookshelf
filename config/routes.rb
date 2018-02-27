788b86792a7a2c95643540e6a8edaa0ec22d705b
Phoebe Lau <phoebelau7@gmail.com>
2018-02-27 18:10:24 -0500

    added header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:index, :show, :new, :create, :edit, :update]
   resources :genres, only: [:index, :show, :new, :create, :edit, :update]
-  get '/users/:id', to: 'users#show'
+
 
   resources :users, only: [:show] do
   # nested resource for reading_lists
     resources :reading_lists, only: [:show]
   end
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
