87362aefb5db4eceab61116a6fe2dfd451c59ac4
Phoebe Lau <phoebelau7@gmail.com>
2018-02-27 23:57:05 -0500

    added show pages for all models

--- app/controllers/reading_lists_controller.rb
+++ app/controllers/reading_lists_controller.rb
@@ -2,4 +2,8 @@ class ReadingListsController < ApplicationController
   def index
     @readinglists = ReadingList.all
   end
+
+  def show
+    @readinglist = ReadingList.find(params[:id])
+  end
 end
