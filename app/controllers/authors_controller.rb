b5cd62412b78104b9be124bbe2dd9db57bc53b2e
Phoebe Lau <phoebelau7@gmail.com>
2018-02-28 12:55:28 -0500

    added controller actions for author class

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,37 @@ class AuthorsController < ApplicationController
     @authors = Author.all
   end
 
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+       redirect_to author_path(@author)
+    end
+  end
+
   def show
     @author= Author.find(params[:id])
+  end
+
+  def edit
+    @author= Author.find(params[:id])
+  end
+
+  def update
+    @author= Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
   end
 end
