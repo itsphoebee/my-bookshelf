1f2654d307d3b5b47a5fa8975ab190431f6aec7e
Phoebe Lau <phoebelau7@gmail.com>
2018-05-04 12:40:00 -0400

    refactored js code for books

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -1,68 +1,48 @@
-// $(function bindEventListeners() {
+// $(function attachListeners() {
 //   $(".js-next").on("click", function(e) {
 //     e.preventDefault();
-//     nextAuthor(this);
+//     nextAuthor();
 //   })
 //
 //   $(".js-previous").on("click", function(e) {
 //     e.preventDefault();
-//     previousAuthor(this);
+//     previousAuthor();
 //   })
 // })
 //
-// function nextAuthor(author) {
-//   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-//   $.get("/authors/" + nextId + ".json", function(data) {
-//   $(".authorName").text(data["name"]);
-//
-//   var books = data["books"]
-//   var bookList = ""
-//   books.forEach(function(book){
-//     bookList += "<li>" + `<a href="/books/${book.id}">` + book.title+ `</a>` + " | " + book.publication_year + "</li>"
-//   })
-//   $(".books").html(bookList);
-//
-//   var genres = data["genres"]
-//   // check for unique genres to list
-//   var uniq = new Set(genres.map(e => JSON.stringify(e)));
-//   var uniqGenres = Array.from(uniq).map(e => JSON.parse(e));
-//   var genreList = ""
-//   uniqGenres.forEach(function(genre){
-//     genreList += `<a href="/genres/${genre.id}">` + genre.name+ `</a>` + " | "
-//   })
-//     genreList += "<br></br>"
-//     $(".affiliatedGenres").html(genreList);
-//
-//     // reset the id to current on the link
-//     $(".js-next").attr("data-id", data["id"]);
-//     $(".js-previous").attr("data-id", data["id"]);
-//   })
+// function nextAuthor() {
+//   var id = parseInt($(".js-next").attr("data-id")) + 1;
+//   getInfo(id)
 // }
 //
 // function previousAuthor () {
-//   var lastId = parseInt($(".js-next").attr("data-id")) - 1;
-//   $.get("/authors/" + lastId + ".json", function(data) {
+//   var id = parseInt($(".js-previous").attr("data-id")) - 1;
+//   getInfo(id)
+// }
+//
+// function getInfo(id){
+//   $.get("/authors/" + id + ".json", function(data) {
 //     $(".authorName").text(data["name"]);
 //
-//     var books = data["books"]
+//     // find all books by specific author and add to view
+//     var books = data.books
 //     var bookList = ""
 //     books.forEach(function(book){
-//       var test = JSON.stringify(book)
-//       bookList += "<li>" + `<a href="/books/${book.id}">` + book.title+ `</a>` + " | " + book.publication_year + "</li>"
+//       bookList += `<li> <a href="/books/${book.id}"> ${book.title} </a> | ${book.publication_year} </li>`
 //     })
 //     $(".books").html(bookList);
 //
-//     var genres = data["genres"]
+//     // find only unique genres then add to view
+//     var genres = data.genres
 //     var uniq = new Set(genres.map(e => JSON.stringify(e)));
 //     var uniqGenres = Array.from(uniq).map(e => JSON.parse(e));
 //     var genreList = ""
 //     uniqGenres.forEach(function(genre){
-//       genreList += `<a href="/genres/${genre.id}">` + genre.name+ `</a>` + " | "
+//       genreList += `<a href="/genres/${genre.id}"> ${genre.name} </a> | <br></br>`
 //     })
-//     genreList += "<br></br>"
 //     $(".affiliatedGenres").html(genreList);
 //
-//     // reset the id to current on the link
+//     // reset the id to current id on the link
 //     $(".js-next").attr("data-id", data["id"]);
 //     $(".js-previous").attr("data-id", data["id"]);
 //   });
