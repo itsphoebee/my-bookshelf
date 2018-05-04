0cd3e47515c99b5f1688e4408b60ceeea30537dd
Phoebe Lau <phoebelau7@gmail.com>
2018-05-04 14:31:46 -0400

    moved js from html.erb to js files

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,38 +1,80 @@
-// $(function bindEventListeners() {
-//   $(".js-more").on("click", function(e) {
-//     e.preventDefault();
-//     showFullReview(this);
-//   })
-//
-//   $(".load-reviews").on("click", function(e) {
-//     e.preventDefault();
-//     loadAllReviews(this);
-//   })
-// })
-//
-// function showFullReview(review) {
-//   var bookId = parseInt($(".bookTitle").attr("data-bookId"));
-//   var reviewId = parseInt($(this).attr("data-id"))
-//   $.get("/books/" + bookId + ".json", function(data) {
-//     var reviews = data.reviews;
-//     var reviewContent = ""
-//     reviews.forEach(function(review){
-//       if (review.id === reviewId){
-//         reviewContent += review.content
-//       }
-//     })
-//     $("#content-" + reviewId).html(reviewContent);
-//   });
-// }
-//
-// function loadAllReviews(review) {
-//   var bookId = parseInt($(".bookTitle").attr("data-bookId"));
-//   $.get("/books/" + bookId + "/reviews" + ".json", function(data) {
-//     var reviews = data
-//     var allReviews = ""
-//     reviews.forEach(function(review){
-//       allReviews += "<strong>" + review.user.email + "</strong>" + " said " + review.content + "<hr>"
-//     })
-//     $("#reviews").html(allReviews);
-//   })
-// }
+$(document).on('turbolinks:load', () => {
+  attachListeners()
+})
+
+function attachListeners() {
+  $(".add-review").on("click", function(e) {
+    e.preventDefault();
+    getForm();
+  });
+
+  $(".load-reviews").on("click", function(e) {
+    e.preventDefault();
+    loadAllReviews()
+  });
+
+  $('#review_form').on("submit", function(e){
+    e.preventDefault();
+    submitReview(this)
+  });
+
+  $(".js-more").on("click", function(e) {
+    e.preventDefault();
+    showReview(this);
+  });
+}
+
+function getForm() {
+  var x = document.getElementById("newreview")
+  x.style.display = "block"
+}
+
+function loadAllReviews() {
+  var bookId = parseInt($(".bookTitle").attr("data-bookId"));
+  $.get("/books/" + bookId + "/reviews" + ".json", function(data) {
+    var reviews = data
+    var allReviews = ""
+    reviews.forEach(function(review){
+      currentReview = new Review(review)
+      allReviews += currentReview.renderReviewContent()
+    })
+    $("#reviews").html(allReviews);
+  })
+};
+
+function showReview(review) {
+  var bookId = parseInt($(".bookTitle").attr("data-bookId"));
+  var reviewId = parseInt($(review).attr("data-id"))
+  $.get("/books/" + bookId + ".json", function(data) {
+    var reviews = data.reviews;
+    reviews.forEach(function(review){
+      if (review.id === reviewId){
+        currentReview = new Review(review)
+      }
+    })
+    $("#content-" + reviewId).html(currentReview.renderReviewContent());
+  });
+}
+
+function submitReview(review){
+  url = review.action
+  $.post(url,$(review).serialize())
+  .done(function(response){
+    debugger
+    // formatting is off - WIP
+    // q about adding edit/delete review/ not so fluid
+    // if (response.split(" said")[0] === $("#user").text()){
+    //   const rights = `<a href="#">Edit Review</a> | <a href="#">Destroy Review</a> | `
+    //   $("#reviews").append(rights)
+    // }
+    $("#reviews").append(response).append("<hr>")
+    clearForm();
+  })
+}
+
+function clearForm(){
+  var x = document.getElementById("newreview")
+  $("#review_content").val("")
+  x.style.display = "none"
+  $('input[type="submit"]').prop('disabled', false);
+}
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,25 +1,10 @@
-// class Review {
-//   constructor() {
-//
-//   }
-//
-// }
-
-// $(function() {
-//   $('#review_form').on("submit", function(e){
-//     e.preventDefault();
-//     postNewReview(this);
-//   });
-// });
-//
-// function postNewReview(review){
-//     url = this.action
-//     $.post(url,$(this).serialize())
-//     .done(function(response){
-//       $("#reviews").append(response).append("<hr>")
-//       var x = document.getElementById("newreview")
-//       $("#review_content").val("")
-//       x.style.display = "none"
-//       $('input[type="submit"]').prop('disabled', false);
-//     })
-//   }
+class Review {
+  constructor(review){
+    this.id = review.id
+    this.content = review.content
+    this.user = review.user.email
+  }
+}
+Review.prototype.renderReviewContent = function(){
+  return `<strong> ${this.user} </strong> said ${this.content} <hr>`
+}
